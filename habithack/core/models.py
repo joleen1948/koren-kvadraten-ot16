dae3a4b6b3a6f365ca46362fa2622ea75c4a9444
joleen1948 <jollyyancheva@gmail.com>
2025-03-22 01:00:55 +0200

    kraini promeni

--- habithack/core/models.py
+++ habithack/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+class Reward(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
