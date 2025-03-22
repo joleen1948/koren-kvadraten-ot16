8dcb41fcebe29f1cefad2aa84211f0e269ab33ab
Yana <yana.al.dimitrova@gmail.com>
2025-03-22 03:02:01 +0200

    krai

--- habithack/core/models.py
+++ habithack/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Reward(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def _str_(self):
+        return self.name
+
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def _str_(self):
+        return self.name
